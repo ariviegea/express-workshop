70514cffe9ae2db69ee326a8e37b861fb277f9fa
ariviegea <ariadna.villar.egea@gmail.com>
2019-05-04 15:48:06 +0200

    Create first json and then stringify it

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var formidable = require('express-formidable');
-var app = express();
+var fs = require('fs');
 
+var app = express();
+var yourData = '{"dataID": "This is my data"}';
+
+var jsonObj = JSON.parse(yourData);
 
 app.use(express.static("public"));
 
@@ -14,3 +18,6 @@ app.listen(3000,function(){
     console.log("Server is listening to port 3000");
 });
 
+fs.writeFile("post.json", yourData,'utf8', (error)=> {
+    console.log(JSON.stringify(jsonObj));
+});
